7e1b9a4ac6be733c34e128eed136fef0e284d41c
Hugo Ng <z5358562@ad.unsw.edu.au>
2024-06-19 19:58:25 +1000

    db

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify
 from models import setup_db
+from src.auth import RegisterAPI, LoginAPI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World!"
+app.add_url_rule('/register', view_func=RegisterAPI.as_view('register_api'), methods=['POST'])
+app.add_url_rule('/login', view_func=LoginAPI.as_view('login_api'), methods=['POST'])
+
+
+
 
 if __name__ == '__main__':
-    setup_db()
+    # setup_db()
     app.run(debug=True, use_reloader=False)
--- backend/src/auth.py
+++ backend/src/auth.py
@@ -1,2 +1,43 @@
-def stub():
-    pass
+from flask import Flask, request, jsonify
+from flask.views import MethodView
+from models import session, User
+from sqlalchemy import select
+import hashlib
+import os 
+
+class RegisterAPI(MethodView):
+    def post(self):
+        data = request.json
+        
+        user = data['username']
+        password = data['password']
+        
+        salt = os.getenv("SALT")
+        hashed_password = hashlib.sha512((password + salt).encode('UTF-8')).hexdigest()
+
+        session.add(User(username=user, password=hashed_password))
+        session.commit()
+        
+        return jsonify({"message": "User registered successfully."}), 201
+
+class LoginAPI(MethodView):
+    def post(self):
+        data = request.json
+        
+        username = data['username']
+        password = data['password']
+        
+        salt = os.getenv("SALT")
+        hashed_password = hashlib.sha512((password + salt).encode('UTF-8')).hexdigest()
+
+        # user = User.query().get(username=username)
+        sql = select(User).where(User.username==username)
+        user = session.execute(sql)
+   
+        if user.first()[0].password == hashed_password:
+            return jsonify({"message": "User logged in successfully."}), 200
+            
+        return jsonify({"message": "you fucking idiot."}), 400
+        
+      
+        
