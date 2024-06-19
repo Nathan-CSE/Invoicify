a6019962195f74db3b244440433ce75aa8f92341
Hugo Ng <z5358562@ad.unsw.edu.au>
2024-06-20 07:18:14 +1000

    working on change password

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -44,5 +44,27 @@ class LoginAPI(MethodView):
             
         return jsonify({"message": "Your username/ password does not match an entry in our system, create an account instead?"}), 400
         
+        
+class ChangePWAPI(MethodView):
+    def patch(self):
+        data = request.json
+        
+        username = data['username']
+        password = data['password']
+        updated_password = data['updated_password']
+        
+        salt = os.getenv("SALT")
+        hashed_password = hashlib.sha512((password + salt).encode('UTF-8')).hexdigest()
+
+        sql = select(User).where(User.username==username)
+        user = session.execute(sql)
+   
+        if user.first()[0].password == hashed_password:
+            hashed_new = hashlib.sha512((updated_password + salt).encode('UTF-8')).hexdigest()
+            
+            
+            return jsonify({"message": "You have successfully changed your password."}), 200
+            
+        return jsonify({"message": "Your password does not match"}), 400
       
         
