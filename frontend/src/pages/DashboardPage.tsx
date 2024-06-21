482f29ee6ee925937576cc28829f74ffa3ec4ae2
Ethan <ethancao@y7mail.com>
2024-06-21 21:13:45 +1000

    Dashboard layout implemented

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -8,15 +8,14 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
+import { CardActionArea } from '@mui/material';
+import { ReactComponent as TickSvg } from '../assets/validate.svg';
+import { ReactComponent as PenSvg } from '../assets/create.svg';
+import { ReactComponent as SendSvg } from '../assets/send.svg';
 
-const options = [
-  'CREATE',
-  'VALIDATE',
-  'SEND',
-  'MANAGE',
-  'SETTINGS',
-  'DOCUMENTATION',
-];
+import { ReactComponent as ManageSvg } from '../assets/manage.svg';
+import { ReactComponent as CogSvg } from '../assets/settings.svg';
+import { ReactComponent as DocSvg } from '../assets/documentation.svg';
 
 // Current Idea -> For loop to create each grid item and card to make it so we dont have to repeat code
 // Wow yeap
@@ -24,54 +23,61 @@ const options = [
 // - Assign a custom string based on the option value -> Switch case
 // - Issue: Have to integrate cardaction and usenavigate here... so?? figure that out yeah
 
-const bull = (
-  <Box
-    component='span'
-    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-  >
-    •
-  </Box>
-);
+interface SVGDict {
+  [key: string]: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+}
 
-const card = (
-  <React.Fragment>
-    <CardContent>
-      <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
-        Word of the Day
-      </Typography>
-      <Typography variant='h5' component='div'>
-        be{bull}nev{bull}o{bull}lent
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color='text.secondary'>
-        adjective
-      </Typography>
-      <Typography variant='body2'>
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size='small'>Learn More</Button>
-    </CardActions>
-  </React.Fragment>
-);
+const options = {
+  'Create/Upload an Invoice': PenSvg,
+  'Validate an Invoice': TickSvg,
+  'Send an Invoice': SendSvg,
+  'Manage Invoices': ManageSvg,
+  'Account Settings': CogSvg,
+  'Documentation Info': DocSvg,
+};
+
+function generateOptions(options: SVGDict): JSX.Element[] {
+  return Object.entries(options).map(([name, SVG], index) => (
+    <Grid key={index} item>
+      <Card
+        sx={{
+          border: 1,
+          borderRadius: '16px',
+          width: '20rem',
+          height: '18rem',
+          alignContent: 'center',
+          textAlign: 'center',
+          display: 'flex',
+        }}
+      >
+        <CardActionArea>
+          <CardContent>
+            <SVG />
+            <Typography variant='h6' component='div'>
+              {name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
+  ));
+}
 
 function DashboardPage() {
   return (
     <>
       <Navbar></Navbar>
       <Box sx={{ mt: 10 }}>
-        <Grid container spacing={3}>
-          <Grid item xs={4}>
-            <Card variant='outlined'>{card}</Card>
-          </Grid>
-          <Grid item xs={4}>
-            hello
-          </Grid>
-          <Grid item xs={4}>
-            hello
-          </Grid>
+        <Grid
+          container
+          spacing={9}
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {generateOptions(options)}
         </Grid>
       </Box>
     </>
