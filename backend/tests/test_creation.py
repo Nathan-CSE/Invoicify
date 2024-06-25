e3198bc894af4ee372f386cefe636ec2c24329e2
Hugo Ng <z5358562@ad.unsw.edu.au>
2024-06-25 19:32:30 +1000

    tested fully

--- backend/tests/test_creation.py
+++ backend/tests/test_creation.py
@@ -4,13 +4,13 @@ import json
 import os
 import io
 
-from tests.fixtures import client  # Assuming you have a fixture for Flask test client
+from tests.fixtures import client  
 
 CREATION_PATH = '/creation/creationupload'
 
-def test_file_upload(client):
+def test_file_upload_successful(client):
     data = {}
-    data['file'] = (io.BytesIO(b"abcdef"), 'test.pdf')
+    data['files'] = [(io.BytesIO(b"abcdef"), 'test.pdf'),(io.BytesIO(b"abcdef"), 'd.json')]
     
     res = client.post(
         CREATION_PATH,
@@ -22,3 +22,35 @@ def test_file_upload(client):
 
     assert res.status_code == 200
     assert response_body['message'] == 'Files received'
+    
+    
+def test_empty_file_upload_fail(client):
+    data = {}
+    data['files'] = []
+    
+    res = client.post(
+        CREATION_PATH,
+        data=data,
+        content_type='multipart/form-data',
+        follow_redirects=True
+    )
+    response_body = res.get_json()
+
+    assert res.status_code == 400
+    assert response_body['message'] == 'No files found in the request'
+    
+    
+def test_invalid_file_upload_fail(client):
+    data = {}
+    data['files'] = [(io.BytesIO(b"abcdef"), 'test.jpg')]
+    
+    res = client.post(
+        CREATION_PATH,
+        data=data,
+        content_type='multipart/form-data',
+        follow_redirects=True
+    )
+    response_body = res.get_json()
+
+    assert res.status_code == 400
+    assert response_body['message'] == 'File test.jpg is not a PDF or JSON'
