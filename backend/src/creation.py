0a8b7e0f7c5c8fa5d04e173bd223d024c0b71deb
Hugo Ng <z5358562@ad.unsw.edu.au>
2024-06-25 21:03:12 +1000

    changes to creation, need to figure out using thunderclient with this to minimise frontend issues when integrating file upload

--- backend/src/creation.py
+++ backend/src/creation.py
@@ -1,20 +1,33 @@
 from flask import Flask, request, jsonify, make_response
 from flask_restx import Namespace, Resource, fields
+from werkzeug.datastructures import FileStorage
 
 creation_ns = Namespace('creation', description='Operations related to uploading files for creaiton')
 
-
+# upload_parser = creation_ns.parser()
+# upload_parser.add_argument('files', location='files',
+#                            type=FileStorage, required=True)
 @creation_ns.route("/creationupload")
 class CreationUploadAPI(Resource):
-
+    # @creation_ns.doc(
+    # description="Creation endpoint for PDFs and JSONs",
+    # body=creation_upload_fields,
+    # responses={
+    #     200: 'Files received successfully',
+    #     400: 'Bad request',
+    # })
     def post(self):
+        # args = upload_parser.parse_args()
+        # files = args['files']
         if 'files' not in request.files:
             return make_response(jsonify({"message": "No files found in the request"}), 400)
 
         files = request.files.getlist('files')
+        # print(files)
         allowed_extensions = {'pdf', 'json'}
 
         for file in files:
+            print(file)
             if file.filename == '':
                 continue 
 
@@ -27,14 +40,6 @@ class CreationUploadAPI(Resource):
 
 
 
-# creation_upload_fields = creation_ns.model('CreationUpload', {
-#     'files': fields.List(fields.Raw, required=True, description='List of files')
-# })
 
-    # @creation_ns.doc(
-    #     description="Creation endpoint for PDFs and JSONs",
-    #     body=creation_upload_fields,
-    #     responses={
-    #         200: 'Files received successfully',
-    #         400: 'Bad request',
-    #     })
+
+   
--- backend/tests/test_creation.py
+++ backend/tests/test_creation.py
@@ -5,9 +5,31 @@ import os
 import io
 
 from tests.fixtures import client  
+from werkzeug.datastructures import FileStorage
 
 CREATION_PATH = '/creation/creationupload'
 
+
+def test_file_upload_successful(client):
+    file = FileStorage(
+        stream=open("file1.pdf", "rb"),
+        filename="file1.pdf",
+        content_type="application/pdf",
+    )
+    data = {}
+    data['files'] = [file]
+    res = client.post(
+        CREATION_PATH,
+        data=data,  
+        content_type='multipart/form-data',
+        follow_redirects=True
+    )
+    response_body = res.get_json()
+
+    assert res.status_code == 200
+    assert response_body['message'] == 'Files received'
+
+    
 def test_file_upload_successful(client):
     data = {}
     data['files'] = [(io.BytesIO(b"abcdef"), 'test.pdf'),(io.BytesIO(b"abcdef"), 'd.json')]
