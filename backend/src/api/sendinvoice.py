770e4fee32ed32736a4671b135f57b7eb4f7ac77
Hugo Ng <z5358562@ad.unsw.edu.au>
2024-06-28 14:21:11 +1000

    stubs for sending

--- backend/src/api/sendinvoice.py
+++ backend/src/api/sendinvoice.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, make_response
-from flask_restx import Namespace, Resource, fields
+from flask_restx import Namespace, Resource
 from werkzeug.datastructures import FileStorage
+from src.services.upload import handle_file_upload
 
 sendInvoice_ns = Namespace('sendInvoice', description='Operations related to uploading files for creaiton')
 
@@ -10,33 +11,40 @@ upload_parser.add_argument('files', location='files',
 @sendInvoice_ns.route("/sendInvoiceUpload")
 class SendInvoiceUploadAPI(Resource):
     @sendInvoice_ns.doc(
-    description="Invoice for sending endpoint for PDFs and JSONs",
+    description="Endpoint for uploading and sending PDFs/ Json",
     responses={
-        200: 'Files received successfully',
+        200: 'Files received and sent successfully',
         400: 'Bad request',
     })
     @sendInvoice_ns.expect(upload_parser)
     def post(self):
-        if 'files' not in request.files:
-            return make_response(jsonify({"message": "No files found in the request"}), 400)
-
-        files = request.files.getlist('files')
-        allowed_extensions = {'pdf', 'json'}
-
-        for file in files:
-            print(file)
-            if file.filename == '':
-                continue 
-
-            if '.' in file.filename and file.filename.rsplit('.', 1)[1].lower() in allowed_extensions:
-                print(file.filename)
-            else:
-                return make_response(jsonify({"message": f"File {file.filename} is not a PDF or JSON"}), 400)
-
-        return make_response(jsonify({"message": "Files received"}), 200)
-
-
-
-
+        res = handle_file_upload(request)
+        if not res[1] == 200:
+            return res
+        
+        # callsend 
+        # collect all XMLs 
+        # return
+        return make_response(jsonify({"message": f"XMLs created"}), 200)
+    
+# TODO: 
+# @sendInvoice_ns.route("/sendInvoiceFromHistory")
+# class SendInvoiceUploadAPI(Resource):
+#     @sendInvoice_ns.doc(
+#     description="Invoice for sending endpoint for PDFs and JSONs",
+#     responses={
+#         200: 'Files received successfully',
+#         400: 'Bad request',
+#     })
+#     @sendInvoice_ns.expect(upload_parser)
+#     def post(self):
+#         res = handle_file_upload(request)
+#         if not res[1] == 200:
+#             return res
+        
+#         # callsend 
+#         # collect all XMLs 
+#         # return
+#         return make_response(jsonify({"message": f"XMLs created"}), 200) 
 
    
