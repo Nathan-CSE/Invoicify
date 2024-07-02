696f1666aef79de18bcb90a238c7a133c4c2fb45
Hugo Ng <z5358562@ad.unsw.edu.au>
2024-07-02 16:49:31 +1000

    validation upload endpoint, need tests and manually testing

--- backend/src/api/invoice.py
+++ backend/src/api/invoice.py
@@ -1,9 +1,14 @@
+import base64
+
 from flask import request, jsonify, make_response
 from flask_restx import Namespace, Resource, fields, reqparse
+from werkzeug.datastructures import FileStorage
 
 from models import db, Invoice
 from src.services.create_xml import create_xml
 from src.services.utils import token_required, db_insert
+from src.services.validation import ValidationService
+from src.services.upload import handle_xml_uplaod
 
 invoice_ns = Namespace('invoice', description='Operations related to creating invoices')
 
@@ -168,3 +173,32 @@ class History(Resource):
                 return (make_response(jsonify({"message": str(err)}), 400))
 
         return make_response(jsonify([invoice.to_dict() for invoice in sql.all()]), 200)
+    
+    
+upload_parser = invoice_ns.parser()
+upload_parser.add_argument('files', location='files',
+                           type=FileStorage, required=True)
+@invoice_ns.route("/validatenewupload")
+class ValidationAPI(Resource):
+    @invoice_ns.doc(
+    description="Upload endpoint for validation of UBL2.1 XMLs",
+    responses={
+        200: 'Files received successfully',
+        400: 'Bad request',
+    })
+    @invoice_ns.expect(upload_parser)
+    @token_required
+    def post(self, user):
+        res = handle_xml_uplaod(request)
+        if not res[1] == 200:
+            return res
+        file = request.files['files']
+        content = file.read()  # Read the file content
+        encoded_content = base64.b64encode(content).decode('utf-8') 
+        vs = ValidationService()
+
+        return vs.validate_xml(
+            filename=file.filename,
+            content=encoded_content,
+            rules=["AUNZ_PEPPOL_1_0_10"]
+        )
