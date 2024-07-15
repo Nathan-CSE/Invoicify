071aba523513ba363c1d4a963423b88b5a6413f1
Ethan <ethancao@y7mail.com>
2024-07-15 22:58:21 +1000

    Reimplemented preview as there was a bug

--- frontend/src/pages/HistoryPreviewInvoice.tsx
+++ frontend/src/pages/HistoryPreviewInvoice.tsx
@@ -6,24 +6,28 @@ import { Link, useNavigate } from 'react-router-dom';
 
 export default function HistoryPreviewInvoice(props: { token: string }) {
   const location = useLocation();
-  const dataFields = location.state.fields;
   const name = location.state.name;
+  const dataFields = location.state.fields;
 
-  function formatJSON(data: any, indentlvl = 0) {
+  const formatJSON = (dataFields: any, indentLevel = 0) => {
     let formattedString = '';
-    const indent = ' '.repeat(indentlvl * 2);
-    for (const [key, value] of Object.entries(data)) {
+    const indent = ' '.repeat(indentLevel * 2); // Use two spaces for each indent level
+
+    // Checks if its an object and has children to loop through so we recurse
+    // Else we just display the value
+    for (const [key, value] of Object.entries(dataFields)) {
       if (typeof value === 'object') {
         formattedString += `${indent}${key}:\n${formatJSON(
           value,
-          indentlvl + 1
+          indentLevel + 1
         )}`;
       } else {
         formattedString += `${indent}${key}: ${value}\n`;
       }
     }
+
     return formattedString;
-  }
+  };
   return (
     <>
       <Box sx={{ mt: 10, ml: 10 }}>
