9e493631189dc4b96e284b379ccc5e592cbbd1a8
William <dragonsword654@gmail.com>
2024-07-26 21:05:46 +1000

    xm,l to json not working because of null values, removed those.

--- backend/src/api/invoice.py
+++ backend/src/api/invoice.py
@@ -280,6 +280,7 @@ class ValidationAPI(Resource):
         converter = ConversionService()
 
         try:
+            print(json.dumps(invoice.fields))
             xml_content = converter.json_to_xml(json.dumps(invoice.fields), rules)
         except Exception as err:
             print(err)
--- backend/src/services/create_xml.py
+++ backend/src/services/create_xml.py
@@ -1,13 +1,13 @@
 import base64
 from src.services.conversion import ConversionService
-
+import json
 from models import Invoice
 from src.services.utils import db_insert
 from src.services.validation import ValidationService
 
 template = """<?xml version="1.0" encoding="UTF-8"?><Invoice xmlns:cac="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" xmlns:cbc="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2" xmlns="urn:oasis:names:specification:ubl:schema:xsd:Invoice-2"><cbc:CustomizationID>urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:aunz:3.0</cbc:CustomizationID><cbc:ProfileID>urn:fdc:peppol.eu:2017:poacc:billing:01:1.0</cbc:ProfileID><cbc:ID>Invoice01</cbc:ID><cbc:IssueDate>{issue_date}</cbc:IssueDate><cbc:InvoiceTypeCode>380</cbc:InvoiceTypeCode><cbc:Note>Taxinvoice</cbc:Note><cbc:DocumentCurrencyCode>AUD</cbc:DocumentCurrencyCode><cbc:BuyerReference>{note}</cbc:BuyerReference><cac:AccountingSupplierParty><cac:Party><cbc:EndpointID schemeID="0151">{seller_abn}</cbc:EndpointID><cac:PartyName><cbc:Name>{company_name}</cbc:Name></cac:PartyName><cac:PostalAddress><cbc:StreetName>{street_name}</cbc:StreetName><cbc:AdditionalStreetName>{additional_name}</cbc:AdditionalStreetName><cbc:CityName>{city_name}</cbc:CityName><cbc:PostalZone>{post_code}</cbc:PostalZone><cac:Country><cbc:IdentificationCode>AU</cbc:IdentificationCode></cac:Country></cac:PostalAddress><cac:PartyLegalEntity><cbc:RegistrationName>{company_name}</cbc:RegistrationName><cbc:CompanyID schemeID="0151">{seller_abn}</cbc:CompanyID></cac:PartyLegalEntity><cac:PartyTaxScheme><cbc:CompanyID>{seller_abn}</cbc:CompanyID><cac:TaxScheme><cbc:ID>GST</cbc:ID></cac:TaxScheme></cac:PartyTaxScheme></cac:Party></cac:AccountingSupplierParty><cac:AccountingCustomerParty><cac:Party><cbc:EndpointID schemeID="0151">{buyer_abn}</cbc:EndpointID><cac:PartyName><cbc:Name>{buyer_company_name}</cbc:Name></cac:PartyName><cac:PostalAddress><cbc:StreetName>{buyer_street_name}</cbc:StreetName><cbc:AdditionalStreetName>{buyer_additional_name}</cbc:AdditionalStreetName><cbc:CityName>{buyer_city_name}</cbc:CityName><cbc:PostalZone>{buyer_post_code}</cbc:PostalZone><cac:Country><cbc:IdentificationCode>AU</cbc:IdentificationCode></cac:Country></cac:PostalAddress><cac:PartyLegalEntity><cbc:RegistrationName>{buyer_company_name}</cbc:RegistrationName><cbc:CompanyID schemeID="0151">{buyer_abn}</cbc:CompanyID></cac:PartyLegalEntity><cac:PartyTaxScheme><cac:TaxScheme><cbc:CompanyID>{buyer_abn}</cbc:CompanyID><cbc:ID>{tax_name}</cbc:ID></cac:TaxScheme></cac:PartyTaxScheme></cac:Party></cac:AccountingCustomerParty><cac:TaxTotal><cbc:TaxAmount currencyID="AUD">{tax_per}</cbc:TaxAmount><cac:TaxSubtotal><cbc:TaxableAmount currencyID="AUD">{total_tax}</cbc:TaxableAmount><cbc:TaxAmount currencyID="AUD">{tax_per}</cbc:TaxAmount><cac:TaxCategory><cbc:ID>S</cbc:ID><cbc:Percent>{tax_per}</cbc:Percent><cac:TaxScheme><cbc:ID>{tax_name}</cbc:ID></cac:TaxScheme></cac:TaxCategory></cac:TaxSubtotal></cac:TaxTotal><cac:LegalMonetaryTotal><cbc:LineExtensionAmount currencyID="AUD">{total_without_tax}</cbc:LineExtensionAmount><cbc:TaxExclusiveAmount currencyID="AUD">{total_without_tax}</cbc:TaxExclusiveAmount><cbc:TaxInclusiveAmount currencyID="AUD">{total_after_tax}</cbc:TaxInclusiveAmount><cbc:PayableAmount currencyID="AUD">{total_after_tax}</cbc:PayableAmount></cac:LegalMonetaryTotal>{products}</Invoice>
 """
-item_format = '<cac:InvoiceLine><cbc:ID>{item_id}</cbc:ID><cbc:Note></cbc:Note><cbc:InvoicedQuantity unitCode="{unit_code}">{amount_product}</cbc:InvoicedQuantity><cbc:LineExtensionAmount currencyID="AUD">{cost_product}</cbc:LineExtensionAmount><cac:Item><cbc:Description>{item_description}</cbc:Description><cbc:Name>{item_name}</cbc:Name><cac:ClassifiedTaxCategory><cbc:ID>GST</cbc:ID><cbc:Percent>{tax_amount}</cbc:Percent><cac:TaxScheme><cbc:ID>{tax_name}</cbc:ID></cac:TaxScheme></cac:ClassifiedTaxCategory></cac:Item><cac:Price><cbc:PriceAmount currencyID="AUD">{cost_per_product}</cbc:PriceAmount></cac:Price></cac:InvoiceLine>'
+item_format = '<cac:InvoiceLine><cbc:ID>{item_id}</cbc:ID><cbc:InvoicedQuantity unitCode="{unit_code}">{amount_product}</cbc:InvoicedQuantity><cbc:LineExtensionAmount currencyID="AUD">{cost_product}</cbc:LineExtensionAmount><cac:Item><cbc:Name>{item_name}</cbc:Name><cac:ClassifiedTaxCategory><cbc:ID>GST</cbc:ID><cbc:Percent>{tax_amount}</cbc:Percent><cac:TaxScheme><cbc:ID>{tax_name}</cbc:ID></cac:TaxScheme></cac:ClassifiedTaxCategory></cac:Item><cac:Price><cbc:PriceAmount currencyID="AUD">{cost_per_product}</cbc:PriceAmount></cac:Price></cac:InvoiceLine>'
 
 def create_xml(file, user):
     products = ""
@@ -16,7 +16,6 @@ def create_xml(file, user):
             item_id = no,
             item_name = item["item"],
             unit_code = item["unitCode"],
-            item_description = item["description"],
             amount_product = item["quantity"] ,
             cost_per_product = item["unitPrice"],
             cost_product = item["totalPrice"],
@@ -49,16 +48,16 @@ def create_xml(file, user):
     except Exception as e:
         raise Exception(f"Couldn't make UBL: {str(e)}")
     cs = ConversionService()
-    json = {}
+    json_val = {}
     try:
-        json = cs.xml_to_json(content)
+        json_val = cs.xml_to_json(content)
     except Exception as e:
         print(f"ERROR: {e}")
-    invoice = Invoice(name=file["invoiceName"], fields=json,  rule="AUNZ_PEPPOL_1_0_10", user_id=user.id, is_ready=False, is_gui=True)
+    invoice = Invoice(name=file["invoiceName"] + ".xml", fields=json.loads(json_val),  rule="AUNZ_PEPPOL_1_0_10", user_id=user.id, is_ready=False, is_gui=True)
 
     db_insert(invoice)
     
-    return {"filename": invoice.name + ".xml", "invoiceId": invoice.id} 
+    return {"filename": invoice.name, "invoiceId": invoice.id} 
  
 if __name__ == "__main__":
     create_xml("test")
