4350ecdff5bef54104e7dbd22e15f56a725783bf
Keefe <keefevuong123@gmail.com>
2024-08-02 15:31:51 +1000

    Changed github actions to split the tests up. Also changed response value of frontend for download

--- frontend/src/pages/HistoryPreviewInvoice.tsx
+++ frontend/src/pages/HistoryPreviewInvoice.tsx
@@ -111,11 +111,11 @@ export default function HistoryPreviewInvoice(props: { token: string }) {
         // Tried to change this to use saveAs lib, but ran into a bunch of issues trying to format the
         // string into a file that was able to be saved
         const url = window.URL.createObjectURL(
-          new Blob([response.data[0]['message']])
+          new Blob([response.data['message']])
         );
         const link = document.createElement('a');
         link.href = url;
-        console.log(response.data[0]['message']);
+        console.log(response.data['message']);
         link.setAttribute('download', name);
         document.body.appendChild(link);
         link.click();
