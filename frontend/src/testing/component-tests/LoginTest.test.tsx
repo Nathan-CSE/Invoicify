6d0516824fe6ed0764c0cef953cd914a3d5f23e4
Ethan <ethancao@y7mail.com>
2024-08-03 18:43:16 +1000

    Some tests

--- frontend/src/testing/component-tests/LoginTest.test.tsx
+++ frontend/src/testing/component-tests/LoginTest.test.tsx
@@ -3,19 +3,50 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SignIn from '../../pages/UserAuth/SignIn';
 import axios from 'axios';
-import { Router } from 'react-router-dom';
+import { MemoryRouter as Router } from 'react-router-dom';
 
 describe('Signin', () => {
+  //   screen.debug();
+
   const noop = () => {};
-  beforeEach(() => {
-    <Router>
-      render(
-      <SignIn token='' setToken={noop} />
-      );
-    </Router>;
+
+  it('renders the page and has the text fields', () => {
+    render(
+      <Router>
+        <SignIn token='' setToken={noop} />
+      </Router>
+    );
+
+    // screen.logTestingPlaygroundURL();
+    expect(
+      screen.getByRole('heading', {
+        name: /sign in/i,
+      })
+    );
+
+    const password = screen.getByLabelText(/password/i);
+    expect(password).toBeInTheDocument();
+    const email = screen.getByRole('textbox', {
+      name: /email/i,
+    });
+    expect(email).toBeInTheDocument();
   });
 
-  it('renders a login button', () => {});
+  it('click on the button and registers it', async () => {
+    const mockSignIn = jest.fn();
+    render(
+      <Router>
+        <SignIn token='' setToken={noop} />
+      </Router>
+    );
+
+    const SignInButton = screen.getByRole('button', {
+      name: /sign in/i,
+    });
+    expect(SignInButton).toBeInTheDocument();
+    fireEvent.click(SignInButton);
+    expect(mockSignIn).toHaveBeenCalled();
+  });
 
   //   it('shows error message when submitting with empty fields', async () => {
   //     const signInButton = screen.getByRole('button', { name: 'Sign In' });
